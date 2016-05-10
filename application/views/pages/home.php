da6ab8bd016ddbd57cebf612f4883966cc43566a
4johndoe <bogdandemchenko@gmail.com>
2016-05-10 13:12:22 +0300

    add structure

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,9 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <div class="pattern1"></div>
 	   <div class="wrap">
 	   	<div class="lang">
-			<a class="lang1" href="./en/"></a>
-			<a class="lang2" href=""></a>
-			<a class="lang3" href="./pl/"></a>
+			<a class="lang1" href="#"></a>
+			<a class="lang2" href="#"></a>
+			<a class="lang3" href="#"></a>
 		</div>
 	   </div>
 
