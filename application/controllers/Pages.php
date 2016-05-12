059a32b2bf689e886ba2916aa78e0b5d40617d81
4johndoe <bogdandemchenko@gmail.com>
2016-05-12 21:26:20 +0300

    pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,16 @@ class Pages extends CI_Controller {
 	public function index()
 	{
             $this->load->view('templates/header');
+            $this->load->view('templates/lang');
             $this->load->view('pages/home');
             $this->load->view('templates/footer');
 	}
+        
+        public function submenu()
+        {
+            $this->load->view('templates/header');
+            $this->load->view('templates/lang');
+            $this->load->view('pages/submenu');
+            $this->load->view('templates/footer');
+        }
 }
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,50 +2,42 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-  <div class="pattern1"></div>
-	   <div class="wrap">
-	   	<div class="lang">
-			<a class="lang1" href="#"></a>
-			<a class="lang2" href="#"></a>
-			<a class="lang3" href="#"></a>
-		</div>
-	   </div>
 
-	 <section id="fringe">
-	   <div class="wrap">
-	     <ul class="row clearfix">
-	       <li class="first col2">
-	         <!-- <span class="img_wrap"><a href="/fringe/#event_charles"><img src="images/kickstarter.png" /></a></span> -->
-                   <span class="img_wrap"><a href="#"><img src="<?php echo base_url(); ?>images/truck.jpg" /></a></span>
-	         <span class="desc_wrap">
-	           <h4>An Evening With</h4>
-  	           <h3>Автомобильные грузоперевозки</h3>
-    	       <p>In this lecture, Charles Adler, co-founder of Kickstarter, will tell us all about Kickstarter&#8217;s past, present, and&nbsp;future.</p>
-	         </span>
-	       </li>
-	       
-	       <li class="last col2">
-	         <!-- <span class="img_wrap"><a href="/fringe/#event_openbookexam"><img src="images/exam.png" /></a></span> -->
-	         <span class="img_wrap"><a href="#"><img src="<?php echo base_url(); ?>images/train.png" /></a></span>
-	         <span class="desc_wrap">
-	           <h4>The Standardistas&#8217;</h4>
-	           <h3>Ж/Д перевозки</h3>
-    	       <p>Requiring you to avail of the web&#8217;s wealth of knowledge to answer the quiz&#8217;s formidable questions, a uniquely difficult&nbsp;challenge.</p>
-	         </span>
-	       </li>
-	     </ul>
-	   </div>
-	 </section>
-	 <section id="sponsors">
-	 	 <div class="wrap">
-	 	 		<h4>Наши партнёры и клиенты</h4>
-	 	 		<ul class="row clearfix">
-	 	 			<li class="first"><a href="#"><img src="<?php echo base_url(); ?>images/sp/hlebinvest.jpg" alt="Mailchimp" /></a></li>
-	 	 			<li><a href="#"><img src="<?php echo base_url(); ?>images/sp/logo.png" alt="Dropbox" /></a></li>
-	 	 			<li><a href="#"><img src="<?php echo base_url(); ?>images/sp/apg.jpg" alt="Shopify" /></a></li>
-	 	 			<li><a href="#"><img src="<?php echo base_url(); ?>images/sp/kormaukraine.jpg" alt="Heroku" /></a></li>
-	 	 			<li class="last"><a href="#"><img src="<?php echo base_url(); ?>images/sp/mhp2.jpg" alt="Typecast" /></a></li>
-	 	 			<li><a href="#"><img src="<?php echo base_url(); ?>images/sp/investreidingservise.jpg" alt="The Manual" /></a></li>
-	 	 		</ul>
-	 	 </div>
-	 </section>
+<section id="fringe">
+  <div class="wrap">
+    <ul class="row clearfix">
+      <li class="first col2">
+        <!-- <span class="img_wrap"><a href="/fringe/#event_charles"><img src="images/kickstarter.png" /></a></span> -->
+          <span class="img_wrap"><a href="#"><img src="<?php echo base_url(); ?>images/truck.jpg" /></a></span>
+        <span class="desc_wrap">
+          <h4>An Evening With</h4>
+          <h3>Автомобильные грузоперевозки</h3>
+      <p>In this lecture, Charles Adler, co-founder of Kickstarter, will tell us all about Kickstarter&#8217;s past, present, and&nbsp;future.</p>
+        </span>
+      </li>
+
+      <li class="last col2">
+        <!-- <span class="img_wrap"><a href="/fringe/#event_openbookexam"><img src="images/exam.png" /></a></span> -->
+        <span class="img_wrap"><a href="#"><img src="<?php echo base_url(); ?>images/train.png" /></a></span>
+        <span class="desc_wrap">
+          <h4>The Standardistas&#8217;</h4>
+          <h3>Ж/Д перевозки</h3>
+      <p>Requiring you to avail of the web&#8217;s wealth of knowledge to answer the quiz&#8217;s formidable questions, a uniquely difficult&nbsp;challenge.</p>
+        </span>
+      </li>
+    </ul>
+  </div>
+</section>
+<section id="sponsors">
+        <div class="wrap">
+                       <h4>Наши партнёры и клиенты</h4>
+                       <ul class="row clearfix">
+                               <li class="first"><a href="#"><img src="<?php echo base_url(); ?>images/sp/hlebinvest.jpg" alt="Mailchimp" /></a></li>
+                               <li><a href="#"><img src="<?php echo base_url(); ?>images/sp/logo.png" alt="Dropbox" /></a></li>
+                               <li><a href="#"><img src="<?php echo base_url(); ?>images/sp/apg.jpg" alt="Shopify" /></a></li>
+                               <li><a href="#"><img src="<?php echo base_url(); ?>images/sp/kormaukraine.jpg" alt="Heroku" /></a></li>
+                               <li class="last"><a href="#"><img src="<?php echo base_url(); ?>images/sp/mhp2.jpg" alt="Typecast" /></a></li>
+                               <li><a href="#"><img src="<?php echo base_url(); ?>images/sp/investreidingservise.jpg" alt="The Manual" /></a></li>
+                       </ul>
+        </div>
+</section>
