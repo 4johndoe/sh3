bc8fe45d0d1ec9b2624f47b8c85da2a54262a064
4johndoe <bogdandemchenko@gmail.com>
2016-05-13 09:31:24 +0300

    lang exclude

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,16 @@ class Pages extends CI_Controller {
 	public function index()
 	{
             $this->load->view('templates/header');
+            $this->load->view('pages/lang');
             $this->load->view('pages/home');
             $this->load->view('templates/footer');
 	}
+        
+        public function truckMenu()
+        {
+            $this->load->view('templates/header');
+            $this->load->view('pages/lang');
+            $this->load->view('pages/truckMenu');
+            $this->load->view('templates/footer');
+        }
 }
